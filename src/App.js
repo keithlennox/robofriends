53b17f8ec54ff847ebbdaae0def884345a8daa1c
klennox <klennox@tvo.org>
2019-10-13 17:38:21 -0400

    End of lesson 203. Building A React App 3

--- src/App.js
+++ src/App.js
@@ -8,16 +8,25 @@ class App extends Component {
         super()
         this.state = {
             robots: robots,
-            searchfield: ''
+            searchfield: '' //The search field is initially blank
         }
     }
-    onSearchChange()
+
+    //Function that fires whenever there is a chnage in the search form field
+    onSearchChange = (event) => { //The event object is passed to this function
+        this.setState({searchfield: event.target.value }) //The value typed into the search field is retrieved from the event object and used to update the state searchfield using setState.
+    }
+
+    //
     render() {
+        const filteredRobots = this.state.robots.filter(robots => { //Built in javascript filter method is applied to the robots object, it takes one argument which is an arrow function.
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //The arrow function returns any names in the robots object that contain the value typed in the search field.
+        }) 
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={this.state.robots}/>
+                <SearchBox searchChange={this.onSearchChange}/> {/*onSearchChange function is passed as a prop to the SeachBox component*/}
+                <CardList robots={filteredRobots}/>
             </div>
         )
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => { //The onSearchChange function is retrieved from the props object using destructuring
     return (
         <div className='pa2'>
             <input 
-                className='pa3 ba b--green bg-lightest-blue'
+                className='pa3 ba b--green bg-lightest-blue' //These are tachyons package values
                 type='search' 
                 placeholder='search robots' 
+                onChange={searchChange} //The onSearchChange function is used as the value for the HTML onChange event handler
             />
         </div>
     )
