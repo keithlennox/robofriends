b6083c928c25abef4b6336409ddf28bf158e70cd
klennox <klennox@tvo.org>
2019-10-12 20:59:43 -0400

    50% done 203. Building A React App 3

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots }  from './robots'; //This is using destructuring to destructure props. I need to learn destructuring.
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    onSearchChange()
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={this.state.robots}/>
+            </div>
+        )
+    }
+}
+
+export default App;
