fcf88a72cf62503c6cda332581c2030099fc2c84
klennox <klennox@tvo.org>
2019-10-13 20:46:57 -0400

    End of lesson 207. Building A React App 6

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -20,20 +20,20 @@ class App extends Component {
             .then(users => {this.setState({ robots: users})});
     }
 
-    //Function that fires whenever there is a chnage in the search form field
+    //Function that fires whenever there is a change in the search form field
     onSearchChange = (event) => { //The event object is passed to this function
         this.setState({searchfield: event.target.value }) //The value typed into the search field is retrieved from the event object and used to update the state searchfield using setState.
     }
 
     //
     render() {
-        const filteredRobots = this.state.robots.filter(robots => { //Built in javascript filter method is applied to the robots object, it takes one argument which is an arrow function.
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //The arrow function returns any names in the robots object that contain the value typed in the search field.
+        const { robots, searchfield } = this.state; //This is getting robots and searchfield from props by using destructuring.
+        const filteredRobots = robots.filter(robot => { //Built in javascript filter method is applied to the robots object, it takes one argument which is an arrow function.
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase()) //The arrow function returns any names in the robots object that contain the value typed in the search field.
         }) 
-        if (this.state.robots.length  === 0) {
-            return <h1>Loading...</h1>
-        }else {
-            return (
+        return !robots.length ? //This is using ternary to do an if else statement. If there are no users returned yet from the APi call, display loading..., else dispaly the users. "!robots.length" is the same as "robots.length ==== 0".
+            <h1>Loading...</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1> {/*className uses tachyons*/}
                     <SearchBox searchChange={this.onSearchChange}/> {/*onSearchChange function is passed as a prop to the SeachBox component*/}
@@ -44,6 +44,5 @@ class App extends Component {
             )
         }
     }
-}
 
 export default App;
